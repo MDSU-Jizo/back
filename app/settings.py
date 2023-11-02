bf496c7dc842f444a9d35c3bfa3e9e06f2e5ece1
Axel Pion <pionaxel@gmail.com>
2023-11-02 15:21:04 +0100

    refactor: trying to update settings to make the ci work

--- app/settings.py
+++ app/settings.py
@@ -125,10 +125,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env('POSTGRES_DB'),
-        'USER': env('POSTGRES_USER'),
-        'PASSWORD': env('POSTGRES_PASSWORD'),
-        'HOST': env('POSTGRES_HOST'),
-        'PORT': env('POSTGRES_PORT'),
+        'USER': env('POSTGRES_USER', 'postgres'),
+        'PASSWORD': env('POSTGRES_PASSWORD', ''),
+        'HOST': env('POSTGRES_HOST', 'localhost'),
+        'PORT': env('POSTGRES_PORT', '5432'),
     }
 }
 
